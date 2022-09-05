88709425cb15b9a4d532c7ddf904bcf0dc42a980
HPCarey <hannahpcarey@gmail.com>
2022-09-05 21:41:52 +0000

    feat: add lives file and create hangman image

---
+++ lives.py
@@ -0,0 +1,79 @@
+print("+------------+")
+print("|            |")
+print("|            O")
+print("|           /|\ ")
+print("|           / \ ")
+print("|")
+print("|")
+print("+-------+")
+
+print("+-----------+")
+print("|           |")
+print("|           O")
+print( " |         /|\ ")
+print("|          / ")
+print("|")
+print("|")
+print("+-------+")
+
+print("+------------+")
+print("|            |")
+print("|            O")
+print("|           /|\ ")
+print("|              ")
+print("|")
+print("|")
+print("+-------+")
+     
+print("+------------+")
+print("|            |")
+print("|            O")
+print("|           /|")
+print("|              ")
+print("|")
+print("|")
+print("+-------+")
+
+print("+------------+")
+print("|            |")
+print("|            O")
+print("|            |")
+print("|              ")
+print("|")
+print("|")
+print("+-------+")
+   
+print("+------------+")
+print("|            ")
+print("|            ")
+print("|            ")
+print("|              ")
+print("|")
+print("|")
+print("+-------+")
+
+       
+lives_left = [
+        """
+        ___________
+        |/        |
+        |         O
+        |        /|\\
+        |         |
+        |        / \\
+        |\\
+        ========
+        """,
+        """
+        ___________
+        |/        |
+        |         O
+        |        /|\\
+        |         |
+        |        /
+        |\\
+        ========
+        """,
+]
+
+print(lives_left)
