7960891416fe1b1dd1707e54ffb33c4c2ab2273f
HPCarey <hannahpcarey@gmail.com>
2022-09-05 21:44:09 +0000

    docs: add sources to lives.py for hangman images

--- lives.py
+++ lives.py
@@ -1,3 +1,6 @@
+"""
+hangman image sources: https://www.youtube.com/watch?v=ynwB-QfOPRw&ab_channel=ShaunHalverson
+https://github.com/SaranSundar/PythonCurriculum/blob/main/Hangman/complete_hangman.py"""
 print("+------------+")
 print("|            |")
 print("|            O")
@@ -52,7 +55,9 @@ print("|")
 print("|")
 print("+-------+")
 
-       
+ """
+ Hangman image source: https://www.youtube.com/watch?v=m4nEnsavl6w&ab_channel=Kite
+ https://github.com/kiteco/python-youtube-code/blob/master/build-hangman-in-python/hangman.py"""      
 lives_left = [
         """
         ___________
