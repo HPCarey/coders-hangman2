3ef6f48a216f9ad9b9278d6914d6f5ca5921fc33
HPCarey <hannahpcarey@gmail.com>
2022-09-05 22:10:56 +0000

    feat: start input validation function to evaluate user's input

--- hangman.py
+++ hangman.py
@@ -17,8 +17,33 @@ def display_word(word):
     print(f'Your word is {len(word)} letters long: {unknown_word}\n')
     return unknown_word
 
+"""
+Checks the user has typed only one alphabet character and not 
+multiple charaters and/or numbers
+"""
+def validate_user_input():
+    valid_input = False
+    letter = ""
+    while valid_input is False:
+        letter = input("Guess a letter: \n")
+        letter = letter.strip().upper()
+        """
+        CI VIDEO, TRY STATEMENT ?
+        """
+        if len(letter) <= 0 or len(letter) > 1:
+            print("Letter must be one character, not more or less")
+        elif letter.isalpha():
+            valid_input = True
+            print("Input correct")    
+        else:
+            print("Letter must be an alphabet character")
+    return letter        
+         
+
+
 def main():
     get_word()
     display_word('word')
+    validate_user_input()
 
 main()
